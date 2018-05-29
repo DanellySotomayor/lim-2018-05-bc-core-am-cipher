3baac9106c5ab26501567cf392c656269d529964
Danelly <danelly.lucia@gmail.com>
2018-05-29 12:52:57 -0500

    subiendo cifrado y cambios en html

--- src/cipher.js
+++ src/cipher.js
@@ -1,16 +1,29 @@
 window.cipher = {
 
-encode:function(offset, string){
-  let mensajeCifrado = '';
+  encode:(offset, string) => {
+      offset = parseInt(offset); //para obtener el número
+      let mensajeCifrado = '';
 
-  for (let index = 0; index < string.lenght; index++){
-      let codigoLetra = string.charCodeAt(index);
-      let codigoCifrado = (codigoLetra - 65 + offset)%26 + 65;
-      let nuevoCaracter = String.fromCharCode(codigoCifrado);
-      mensajeCifrado = mensajeCifrado + nuevoCaracter;
+      for (let lista = 0; lista < string.length; lista++) { 
+          let codigoLetra = string.toUpperCase().charCodeAt(lista); 
+          let codigoCifrado = (codigoLetra - 65 + offset)%26 + 65;
+          let nuevoCaracter = String.fromCharCode(codigoCifrado); //conviente el nuevo ascii a string
+          mensajeCifrado = mensajeCifrado + nuevoCaracter;
+      }
+      return mensajeCifrado;
+  },
+
+  decode: (offset, string) => {
+    offset = parseInt(offset);
+    let mensajeDescifrado = '';
+
+    for (let lista = 0; lista < string.length; lista++) {
+        let codigoLetra = string.toUpperCase().charCodeAt(); 
+        let codigoCifrado = (codigoLetra + 65 - offset)%26 + 65;
+        let nuevoCaracter = String.fromCharCode(codigoCifrado);
+        mensajeDescifrado += nuevoCaracter;
+      }
+  
+    return mensajeDescifrado;
   }
-
-  return mensajeCifrado;
 }
-
-};
--- src/index.js
+++ src/index.js
@@ -1,7 +1,28 @@
-let codificar = document.getElementById('codificar');
-codificar.addEventListener('click',() => {
-    let offset = document.getElementById('key').value;
-    let ingresaMensaje = document.getElementById('mensajePrincipal').value;
-    let encode = cipher.encode(offset, ingresaMensaje);
-    document.getElementById('resultado').value = encode;
-});
+
+    //boton codificar
+    let codificar = document.getElementById('codificar');
+    codificar.addEventListener('click', () => {
+        let offset = parseInt(document.getElementById('llave').value);
+        let ingresaMensaje = document.getElementById('mensajePrincipal').value;
+        let encode = cipher.encode(offset, ingresaMensaje);
+        document.getElementById('resultado').innerHTML = encode;
+    });
+
+    //boton decodificar
+    let decodificar = document.getElementById('decodificar');
+    codificar.addEventListener('click', () => {
+        let offset = parseInt(document.getElementById('llave').value);
+        let ingresaMensaje = document.getElementById('mensajePrincipal').value;
+        let encode = cipher.decode(offset, ingresaMensaje);
+        document.getElementById('resultado').innerHTML = decode;
+
+    });
+
+    //limpiar las áreas de texto
+    let resetear = document.getElementById('resetear');
+    resetear.addEventListener('click', () => {
+        document.getElementById('llave').value = '';
+        document.getElementById('mensajePrincipal').value = '';
+        document.getElementById('resultado').innerHTML = '';
+   
+     });
