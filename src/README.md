6df90e082f15436e7c74c969ae1ac114f5513a13
Danelly <danelly.lucia@gmail.com>
2018-06-04 08:15:13 -0500

    subiendo cambios en codigo, css y read me

--- src/README.md
+++ src/README.md
@@ -8,14 +8,34 @@ La segunda persona ingresa a la aplicación y, con el número de posiciones que 
 El primer proyecto solicitado en Laboratoria es una aplicación para poder cifrar y descifrar mensajes utilizando el conocido [Cifrado César](https://es.wikipedia.org/wiki/Cifrado_C%C3%A9sar "conoce más sobre el Cifrado César"), método de cifrado por sustitución en que una letra de un texto es reemplazada por otra letra que se encuentra un número fijo de posiciones más adelante en el alfabeto.
 
 ### Objetivos
-
+* Diseñar un producto pensando en los usuarios.
+* Construir una interfaz.
+* Escuchar eventos básicos del DOM.
+* Escribir lógica para llevar a cabo el cifado/descifrado.
+* Tests unitarios básicos para comprobar (y documentar) dicha lógica.
+* Manipulación del DOM para mostrar (escribir) los resultados.
 
 ### Conociendo al usuario
-* Se realizó una lluvia de ideas en el squad para poder conocer más detalles de los usuarios de la aplicación.
-* El usuario a utilizar la aplicación se caracteriza por ser un usuario joven, entre 16 a 28 años, que comparte con experiencias con sus amigos y le gusta la tecnología.
+* Se realizó una lluvia de ideas en el squad para poder conocer más detalles de los usuarios de la aplicación. Adicional a ello, se consultó con posibles usuarios del rango de edades 24 a 28 años sobre sus preferencias en colores, diseño y uso.
+* El usuario a utilizar la aplicación se caracteriza por ser un usuario joven, entre 16 a 28 años, que comparte sus experiencias con sus amigos y le gusta la tecnología.
 
 ### Planteando la interfaz
 * Al ser un usuario joven, busca acceder a sus aplicaciones de manera rápida, por lo que se planteó una  interfaz sencilla e intuitiva.
 * Cuenta con un cuadro para colocar el número de espacios a usar para cifrar el mensaje, un cuadro de texto para ingresar los textos a cifrar y descifrar, tres botones (Cifrar, Descifrar y Limpiar) y un espacio donde muestra el resultado.
 * Asimismo, cuenta con 3 botones que se usarán según el objetivo del usuario: Codificar, Decodificar y Resetear.
+* El color oscuro del fondo se utilizó debido a la consulta previa que se realizó, en donde posibles usuarios indicaron que usarían con mayor frecuencia esta aplicación en horarios nocturnos.
+
+### Modo de uso
+**Cifrado:**
+* Se ingresa a la página https://danellysotomayor.github.io/lim-2018-05-bc-core-am-cipher/src
+* El usuario escribe o pega el mensaje en el cuadro de texto.
+* Escribe el número de posiciones que desea mover para cifrar el mensaje (offset).
+* Presiona el botón "Cifrar" y podrá ver su resultado en la parte inferior. Luego de ello, puede copiarlo y compartirlo con su amigos.
+**Descifrado:**
+* Se ingresa a la página https://danellysotomayor.github.io/lim-2018-05-bc-core-am-cipher/src
+* El usuario pega el mensaje cifrado en el cuadro de texto.
+* Escribe el número de posiciones (offset) que previamente le ha indicado la persona que le mandó el mensaje cifrado.
+* Presiona el botón "Descifrar" y podrá descubrir el mensaje original.
+**Limpiar:**
+* En caso se cometa algún error o se quiera borrar el texto ingresado en un solo paso, se debe presionar el botón "Limpiar".
 
--- src/cipher.js
+++ src/cipher.js
@@ -1,37 +1,59 @@
 window.cipher = {
+  encode: (offset, string) => {
+    let key = parseInt(offset); //definimos la variable 'Key' para obtener el número de saltos en el alfabeto
+    let mensajeCifrado = ''; //definimos variable para obtener el mensaje cifrado final
+    let nuevoCaracter = ''; //definimos variable para obtener la letra cifrada
+      for (let lista = 0; lista < string.length; lista++) { //busca la posición de la letra en el string ingresado
+        let codigoLetra = string.charCodeAt(lista); //convierte a codigo ascii cada letra ingresada
+        if (codigoLetra>=65 && codigoLetra<=90) { //tomamos el rango del alfabeto en mayúscula para operar la fórmula siguiente
+          nuevoCaracter = String.fromCharCode((codigoLetra - 65 + key)%26 + 65); //fórmula que cifra el código ascii según el offset
+          mensajeCifrado += nuevoCaracter; 
+        } else if (codigoLetra >= 97 && codigoLetra <= 122) {
+          nuevoCaracter = String.fromCharCode((codigoLetra - 97 + key)%26 + 97)
+          mensajeCifrado += nuevoCaracter;//devuelve el nuevo caracter en string
+        } else if (codigoLetra >= 48 && codigoLetra <= 57) {
+          nuevoCaracter = String.fromCharCode((codigoLetra - 48 + key)%10 + 48)
+          mensajeCifrado += nuevoCaracter;//devuelve el nuevo caracter en string
+        } else {
+          nuevoCaracter = String.fromCharCode(codigoLetra);
+          mensajeCifrado += nuevoCaracter;
+          }
+      }
+      return mensajeCifrado;    
+  },
+   
+  decode: (offset, string) => {
+    let key = parseInt(offset); 
+    let mensajeDescifrado = '';
+    let nuevoCaracterDos = '';
+      for (let indice = 0; indice < string.length; indice++) {
+        let codigoLetraDos = string.charCodeAt(indice); 
+        if (codigoLetraDos>=65 && codigoLetraDos<=90) {
+          nuevoCaracterDos = String.fromCharCode((codigoLetraDos - 90 - key)%26 + 90);
+          mensajeDescifrado += nuevoCaracterDos;
+        } else if (codigoLetraDos >= 97 && codigoLetraDos <= 122) {
+          nuevoCaracterDos = String.fromCharCode((codigoLetraDos-122 - key)%26 + 122)
+          mensajeDescifrado += nuevoCaracterDos;
+        } else if (codigoLetraDos >= 48 && codigoLetraDos <= 57) {
+          nuevoCaracterDos = String.fromCharCode((codigoLetraDos-57 - key)%10 + 57)
+          mensajeDescifrado += nuevoCaracterDos;
+        } else {
+          nuevoCaracterDos = String.fromCharCode(codigoLetraDos);
+          mensajeDescifrado += nuevoCaracterDos;
+        }
+      }
+      return mensajeDescifrado;
+  },
 
-  encode:(offset, string) => {
-      offset = parseInt(offset); //para obtener el número
-      let mensajeCifrado = '';
+  createCipherWithOffset(offset) {
+    let objetoSolicitado = {
+      encode:(string) => 
+        cipher.encode(offset, string),
 
-      for (let lista = 0; lista < string.length; lista++) { //busca la posición de la letra en el string
-          let codigoLetra = string.toUpperCase().charCodeAt(lista); //convierte a mayúscula y a codigo ascii
-          if (codigoLetra === 32) { //identifica el espacio segun el codigo ascii
-             mensajeCifrado += ' ' //devuelve espacio en string
-          } else {
-          let codigoCifrado = (codigoLetra - 65 + offset)%26 + 65; //fórmula para obtener el nuevo caracter
-          let nuevoCaracter = String.fromCharCode(codigoCifrado); //convierte el nuevo caracter ascii a string
-          mensajeCifrado = mensajeCifrado + nuevoCaracter; //devuelve el nuevo caracter en string
+      decode:(string) => 
+        cipher.decode(offset, string)
     }
+    return objetoSolicitado;
   }
 
-      return mensajeCifrado;
-  },
- 
-  decode: (offset, string) => {
-    offset = parseInt(offset); 
-    let mensajeDescifrado = '';
-
-    for (let indice = 0; indice < string.length; indice++) {
-        let codigoLetraDos = string.toUpperCase().charCodeAt(indice); 
-        if (codigoLetraDos === 32) {
-            mensajeDescifrado += ' '
-       } else {
-        let codigoDescifrado = (codigoLetraDos + 65 - offset)%26 + 65;
-        let nuevoCaracterDos = String.fromCharCode(codigoDescifrado);
-        mensajeDescifrado += nuevoCaracterDos;
-      }
-    }
-    return mensajeDescifrado;
-}
 }
